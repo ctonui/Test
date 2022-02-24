33a8a7f0cd072df5bea455d3bc7b872b7dbc76b7
ctonui <ctonui@gmail.com>
2022-02-24 09:02:36 +0300

    Added a new class-no naming names for now

---
+++ TestAppilication/Repository/Models/Onion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAppilication.Repository.Models
+{
+    public class Onion
+    {
+        public int OnionId { get; set; }
+        public string OnionColour { get; set; }
+        public int Layers { get; set; }
+    }
+}
